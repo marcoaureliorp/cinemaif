b92908d2ac97b2e751e227db3672d9cafd4cbd80
Daniel Moreira <dmcardoso.99@hotmail.com>
2019-08-15 20:36:18 -0300

    global style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import GlobalStyle from "./config/css-reset";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>CinemaIF</h1>
-    </div>
-  );
+    return (
+        <div>
+            <GlobalStyle/>
+            <h1>CinemaIF</h1>
+        </div>
+    );
 }
 
 export default App;
