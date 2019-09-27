07458ed504017ef903b817188b9fb6a874296c82
marcoaureliorp <marcoaurelio.rp@hotmail.com>
2019-09-26 21:35:58 -0300

    Correção de estilo na listagem de Filmes

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Container } from './style';
 import Page from '../../components/page';
 import Filme from '../../components/filme';
-import Input from '../../components/form-components/input';
-import Button from '../../components/button';
 
 function Home() {
     return (
         <Page title="Lançamentos">
-            <Filme />
-            <Input placeholder="Input" />
-            <Button label="Butão" kind="save" />
+            <Container>
+                <Filme />
+                <Filme />
+                <Filme />
+                <Filme />
+                <Filme />
+                <Filme />
+                <Filme />
+            </Container>
         </Page>
     );
 }
